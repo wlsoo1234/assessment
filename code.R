31afd831c7a9354abd2c5a5ecaef29497d310e00
wlsoo1234 <s2148250@siswa.um.edu.my>
2023-01-21 03:43:45 +0800

    Update code.R

--- code.R
+++ code.R
@@ -1,5 +1,17 @@
-install.packages(ranger)
+install.packages("ranger")
+install.packages("caret")
+install.packages("data.table")
 library(ranger)
 library(caret)
 library(data.table)
 creditcard_data <- read.csv("C:/Users/soowe/Documents/creditcard.csv")
+
+dim(creditcard_data)
+head(creditcard_data,6)
+
+table(creditcard_data$Class)
+summary(creditcard_data$Amount)
+names(creditcard_data)
+var(creditcard_data$Amount)
+
+sd(creditcard_data$Amount)
