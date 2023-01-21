9f688c00106401868815704e27cf3104bfaa6b0a
wlsoo1234 <s2148250@siswa.um.edu.my>
2023-01-21 10:14:50 +0800

    Update code.R

--- code.R
+++ code.R
@@ -15,3 +15,28 @@ names(creditcard_data)
 var(creditcard_data$Amount)
 
 sd(creditcard_data$Amount)
+
+View(creditcard_data)
+
+head(creditcard_data)
+
+creditcard_data$Amount=scale(creditcard_data$Amount)
+NewData=creditcard_data[,-c(1)]
+head(NewData)
+
+
+#Data Modeling 
+#After we have standardized our entire dataset, 
+#we will split our dataset into training set as well as test 
+#set with a split ratio of 0.80. This means that 80% of our data 
+#will be attributed to the train_data whereas 20% will be attributed
+#to the test data. We will then find the dimensions using the dim()
+#function -
+install.packages("caTools")
+library(caTools)
+set.seed(123)
+data_sample = sample.split(NewData$Class,SplitRatio=0.80)
+train_data = subset(NewData,data_sample==TRUE)
+test_data = subset(NewData,data_sample==FALSE)
+dim(train_data)
+dim(test_data)
